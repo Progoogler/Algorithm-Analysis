2b53b36e9a7bcc0fb794056b192ac0fc8d435716
Andy Li <Andyzanli@gmail.com>
2016-01-29 21:37:37 -0800

    Update binarySearch.js

--- binarySearch.js
+++ binarySearch.js
@@ -5,31 +5,31 @@
 
 var binarySearch = function(array, target, beg, end) {
 
-  // Set the parameter value of 'beg' on recursive calls
-  // otherwise set its value to 0 if undefined.
-  beg = beg || 0;
-
-  // Similar to above pattern without explicitly
-  // stating that 'end' might equal a new value.
-  if (end === undefined) {
-  	end = array.length-1;
-  }
-
-  // Use the index values of 'beg' and 'end' to
-  // find the middle index in the current array.
-  var mid = beg + Math.floor((end - beg) / 2);
-  var num = array[mid];
-
-  // Base case: return the index of the 'target'
-  // when it matches the 'num' element.
-  if (target === num) {
-	return mid;
+	  // Set the parameter value of 'beg' on recursive calls
+	  // otherwise set its value to 0 if undefined.
+	  beg = beg || 0;
+	
+	  // Similar to above pattern without explicitly
+	  // stating that 'end' might equal a new value.
+	  if (end === undefined) {
+	  	end = array.length-1;
+	  }
+	
+	  // Use the index values of 'beg' and 'end' to
+	  // find the middle index in the current array.
+	  var mid = beg + Math.floor((end - beg) / 2);
+	  var num = array[mid];
+	
+	  // Base case: return the index of the 'target'
+	  // when it matches the 'num' element.
+	  if (target === num) {
+		return mid;
 	
 	} else if (beg >= end) {
-    // If the beginning index becomes greater than
+                // If the beginning index becomes greater than
 		// the end value, the 'target' element is either
-	  // not found or array may not be sorted.
-    return -1
+	        // not found or array may not be sorted.
+                return -1
             
 	} else if (target > num) {
 	    // Recursively search right half of the array
