4ca9632d6fe0f47b74691f1afd21320c32824f01
Andy Li <Andyzanli@gmail.com>
2016-01-31 23:23:17 -0800

    Create quickSort.js

---
+++ quickSort.js
@@ -0,0 +1,69 @@
+// Swap element at 'firstIndex' with 'secondIndex'.
+var swap = function(array, firstIndex, secondIndex) {
+	var temp = array[firstIndex];
+	array[firstIndex] = array[secondIndex];
+	array[secondIndex] = temp;
+};
+
+// Assign a random pivot to array lengths greater than 100
+// otherwise use the middle element despite any possbile event 
+// of worst-case running time.
+var randomPivot = function(array) {
+	if (array.length < 100) {
+		return array.indexOf(array[Math.ceil(array.length / 2)]);
+	}
+	var random = [];
+	num = Math.ceil(Math.random() * 10);
+	random.push(array[Math.ceil(array.length / num)]);
+	random.push(array[array.length - (Math.ceil(array.length / num))]);
+	random.push(array[Math.ceil(array.length / num) + Math.ceil(array.length / num)]);
+	random.sort(function(a,b) {
+		return a+b;
+	});
+	return array.indexOf(random[1]);
+};
+
+// Swap all elements less than the pivot to the left
+// of the pivot and all elements greater than the pivot
+// to the right. Pivot gets swapped last from the array.length - 1
+// position after each loop.
+var partition = function(array, low, high) {
+	// Var 'i' points to the border between all elements
+	// less than and greater than the pivot.
+	var i = low
+	, j = low
+	, pivot = randomPivot(array);
+	swap(array, pivot, high)
+
+	// Loop through array or subarray
+	while (j < high) {
+
+		if (array[j] <= array[high]) {
+			// When current element is left than pivot,
+			// swap it with the pointer "i".
+			swap(array, i, j);
+			// Increment 'i' to point at the next
+			// element greater than the pivot 
+			i++;
+		}
+		j++;
+	}
+	// Swap the pivot with 'i' so that all elements
+	// less than it are to the left and all elements
+	// greater than it are to the right.
+	swap(array, i, high);
+	// Return the index value of 'i' to the recursive call.
+	return i;
+};
+
+// Sort all elements of an array in place without a new array.
+// Changes order of the actual array input 
+var quickSort = function(array, low, high) {
+	if (low < high) {
+		var key = partition(array, low, high);
+		quickSort(array, low, key-1);
+		quickSort(array, key+1, high);
+	} else {
+		return;
+	}
+};
