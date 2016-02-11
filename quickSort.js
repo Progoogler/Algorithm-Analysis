4c5064c267c3a839f2adcde44989c4a74029e31b
Progoogler <Andyzanli@gmail.com>
2016-02-10 21:49:00 -0800

    Fixed pivot error and added test case.

--- quickSort.js
+++ quickSort.js
@@ -5,53 +5,35 @@ var swap = function(array, firstIndex, secondIndex) {
 	array[secondIndex] = temp;
 };
 
-// Assign a random pivot to array lengths greater than 100
-// otherwise use the middle element despite any possbile event 
-// of worst-case running time.
-var randomPivot = function(array) {
-	if (array.length < 100) {
-		return array.indexOf(array[Math.ceil(array.length / 2)]);
-	}
-	var random = [];
-	var num = Math.ceil(Math.random() * 10);
-	random.push(array[Math.ceil(array.length / num)]);
-	random.push(array[array.length - (Math.ceil(array.length / num))]);
-	random.push(array[Math.ceil(array.length / num) + Math.ceil(array.length / num)]);
-	random.sort(function(a,b) {
-		return a+b;
-	});
-	// Return the median of the 3 randomly chosen elements' index.
-	return array.indexOf(random[1]);
-};
-
 // Swap all elements less than the pivot to the left
 // of the pivot and all elements greater than the pivot
-// to the right. Pivot gets swapped last from the array.length - 1
-// position after each loop.
+// to the right. Pivot gets swapped last from the 'high'
+// position into its correct position after the loop.
 var partition = function(array, low, high) {
 	// Var 'i' points to the border between all elements
 	// less than and greater than the pivot.
 	var i = low
-	, j = low
-	, pivot = randomPivot(array);
-	swap(array, pivot, high)
+	    , j = low
+	    // The 'pivot' variable is the middle index between high and low.
+	    , pivot = low + Math.ceil((high - low) / 2);
 
-	// Loop through array or subarray
+	// Swap the 'pivot' element into the 'high' index position to the right.
+	swap(array, pivot, high);
+
 	while (j < high) {
 
-		if (array[j] <= array[high]) {
-			// When current element is less than pivot,
-			// swap it with the pointer "i".
+		if (array[j] < array[high]) {
+			// Swap elements at 'j' index with 'i' when elements less than 
+			// the pivot are found, shifting all elements less than pivot to
+			// the left of the subarray. 
 			swap(array, i, j);
-			// Increment 'i' to point at the next
-			// element greater than the pivot 
 			i++;
 		}
 		j++;
 	}
 	// Swap the pivot with 'i' so that all elements
-	// less than it are to the left and all elements
-	// greater than it are to the right.
+	// less than it are to the left of the 'pivot' and all elements
+	// greater than the 'pivot' are to the right.
 	swap(array, i, high);
 	// Return the index value of 'i' to the recursive call.
 	return i;
@@ -64,11 +46,33 @@ var partition = function(array, low, high) {
 // Note : Sorts the order of the original array reference
 // therefore it returns undefined. 
 var quickSort = function(array, low, high) {
+
+	if (typeof low !== 'number') {
+		low = 0;
+	}
+  // Cases for missing arguments on initial sort call.
+	if (typeof high !== 'number') {
+		high = array.length - 1;
+	}
+
 	if (low < high) {
-		var key = partition(array, low, high);
-		quickSort(array, low, key-1);
-		quickSort(array, key+1, high);
+    // Store the return value of 'i' from partition which holds
+    // the pivot index and recurse on the left and right sides
+    // of the 'pivot'.
+		var pivot = partition(array, low, high);
+		quickSort(array, low, pivot-1);
+		quickSort(array, pivot+1, high);
+
 	} else {
 		return;
 	}
+
 };
+
+
+// Test case.
+var arr;
+for (var n = 0; n < 20; n++) {
+	arr.push(Math.ceil(Math.random() * 100));
+}
+quickSort(arr);
