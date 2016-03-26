9093784484a5e6fca8a4e552a5ddd123c6654a38
Progoogler <Andyzanli@gmail.com>
2016-03-25 23:38:14 -0700

    Removed false attribution

--- selectionSort.js
+++ selectionSort.js
@@ -3,17 +3,15 @@
 * sorting an array of elements. It begins by finding the
 * smallest element in the array and swapping it with the
 * first index, then the second smallest with the second index,
-* and so on. It's time complexity of O(n^2) makes it less
+* and so on. Its time complexity of O(n^2) makes it less
 * ideal for arrays of lengths greater than 20, otherwise for
-* small inputs it can be extremely fast sorting algorithm.
+* small inputs it can be an extremely fast sorting algorithm.
 *
 * Selection Sort is favored in terms of its write operations,
 * which only utilizes O(n) times in its swap routine.
-*
-* Note: Selection Sort is also favored for small arrays
-*  		  that are nearly sorted.
 */
 "use strict";
+
 const selectionSort = array => {
 	const swap = (a,n,m) => {
 		let temp = a[n];
@@ -21,7 +19,7 @@ const selectionSort = array => {
 		a[m] = temp;
 	};
 	let smallestInt,
-			index,
+      index,
 	    len = array.length;
 
   for (let i = 0; i < len-1; i++) {
