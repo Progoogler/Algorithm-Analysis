03a6d5dddd0e2c8edcfdee412f0e0f94357e37d7
Progoogler <Andyzanli@gmail.com>
2016-03-29 22:35:42 -0700

    ES6 update & already sorted optimization

--- bubbleSort.js
+++ bubbleSort.js
@@ -8,98 +8,101 @@
 *
 * Expected output : [3223, 546, 455, 345, 234, 213, 122, 98, 84, 64, 23, 12, 9, 4, 1]
 */
-
+"use strict"
 
 // Recursive solution -- space and time expensive
-var bubbleSort = function(array, index) {
-    index = index || 0;
-    
-    if (index === array.length - 1) {
+let bubbleSort = (array, index) => {
+  index = index || 0;
+  
+  if (index === array.length - 1) {
+    return array;
+  }
+
+  for (let i = 0; i < array.length; i += 1) {
+    //traverse array, comparing numbers next to each other
+    if (array[i] > array[i + 1]) {
+        //shift larger number to the left of the array in descending order
+        swap(array, i, i+1);
+        return bubbleSort(array, i);
+        //recurse function passing in value of i as 'index'
+    } else if (array[i] === array[array.length - 1]) {
         return array;
     }
-
-    for (var i = 0; i < array.length; i += 1) {
-        //traverse array, comparing numbers next to each other
-        if (array[i] > array[i + 1]) {
-            //shift larger number to the left of the array in descending order
-            swap(array, i, i+1);
-            return bubbleSort(array, i);
-            //recurse function passing in value of i as 'index'
-        } else if (array[i] === array[array.length - 1]) {
-            return array;
-      }
-   }
-
+  }
 };
 
-test = [345, 4, 546, 122, 84, 98, 64, 9, 1, 3223, 455, 23, 234, 200];
+let test = [345, 4, 546, 122, 84, 98, 64, 9, 1, 3223, 455, 23, 234, 200];
 bubbleSort(test);
 
 
 // Solution without recursion.
 // O(n^2) with unpredictable coefficients.
-var bubbleSort = function(array) {
-    for (var i = 0; i < array.length; i++) {
-        if (array[i] < array[i + 1]) {
-            swap(array, i, i+1);
-            i = -1;
-        }
+let bubbleSort = array => {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < array[i + 1]) {
+      swap(array, i, i+1);
+      i = -1;
     }
+  }
 
-    return array;
- };
+  return array;
+};
 
- test = [345, 4, 546, 122, 84, 98, 64, 9, 1, 3223, 455, 23, 234, 200];
- bubbleSort(test);
+let test = [345, 4, 546, 122, 84, 98, 64, 9, 1, 3223, 455, 23, 234, 200];
+bubbleSort(test);
 
 
 // Nested for-loop -- most optimal solution.
 // Run time is O(n^2) and blazingly fast for all 
 // inputs less than 10!
-var bubbleSort = function(array) {
-    for (var i = array.length; i > 0; i--) {
-        for (var j = 0; j < i; j++) {
-            if (array[j] < array[j+1]) {
-                swap(array, j, j+1);
-            }
-        }
+let bubbleSort = array => {
+  let swapped;
+  for (let i = array.length; i > 0; i--) {
+    swapped = false;
+    for (let j = 0; j < i; j++) {
+      if (array[j] < array[j+1]) {
+        swap(array, j, j+1);
+        swapped = true;
+      }
     }
-    
-    return array;
+    // Optimization -- return if inner loop does not swap an element
+    if (!swapped) return;
+  }
+  
+  return array;
 };
 
 function swap(array, a, b) {
-    var temp = array[a];
-    array[a] = array[b];
-    array[b] = temp;
+  let temp = array[a];
+  array[a] = array[b];
+  array[b] = temp;
 }
 
-test = [345, 4, 546, 122, 84, 98, 64, 9, 1, 3223, 455, 23, 234, 200];
-bubble(test);
-
+let test = [345, 4, 546, 122, 84, 98, 64, 9, 1, 3223, 455, 23, 234, 200];
+bubbleSort(test);
 
 // W3Resource solution:
 function bubble_Sort(a)
 {
-    var swapp;
-    var n = a.length-1;
-    var j = 0;
-    var x=a;
-    do {
-        swapp = false;
-        for (var i=0; i < n; i++)
-        {
-            if (x[i] < x[i+1])
-            {
-               var temp = x[i];
-               x[i] = x[i+1];
-               x[i+1] = temp;
-               swapp = true;
-            }
-        }
-        n--;
-    } while (swapp);
- return x; 
+  var swapp;
+  var n = a.length-1;
+  var j = 0;
+  var x=a;
+  do {
+      swapp = false;
+      for (var i=0; i < n; i++)
+      {
+          if (x[i] < x[i+1])
+          {
+             var temp = x[i];
+             x[i] = x[i+1];
+             x[i+1] = temp;
+             swapp = true;
+          }
+      }
+      n--;
+  } while (swapp);
+return x; 
 }
 
 console.log(bubble_Sort([345, 4, 546, 122, 84, 98, 64, 9, 1, 3223, 455, 23, 234, 200]));
